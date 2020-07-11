8b27fde9eaff9ba00d424916f724af2bc43fbcff
oswalgopal <gploswal@gmail.com>
2020-07-11 13:19:37 +0530

    added image to pdf

--- src/Pages/ScannedImages.js
+++ src/Pages/ScannedImages.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import {Image, View, ScrollView} from "react-native";
+import {Image, View, ScrollView, TouchableOpacity, Text, Dimensions} from "react-native";
+import RNImageToPdf from 'react-native-image-to-pdf';
+import {lightTheme} from "../Theme/lightTheme";
 
 const ScannedImages = (props) => {
+    const makePdf = async () => {
+        console.log(props.route.params.images);
+        try {
+            const options = {
+                imagePaths:
+                    [
+                        "/data/user/0/com.notespedia/cache/Camera/bb3be46b-8507-44fd-ac9c-7df8e42d4329.jpg",
+                        "/data/user/0/com.notespedia/cache/Camera/5c098b14-023f-4f6f-9adb-40d0dee2a3ad.jpg"
+                    ],
+                name:  'Scanned Name',
+        };
+            const pdf = await RNImageToPdf.createPDFbyImages(options);
+            console.log(pdf);
+        } catch(e) {
+            console.log('error', e);
+        }
+    }
     return (
         <View>
+            <TouchableOpacity onPress={makePdf}>
+                <Text style={{
+                    backgroundColor: lightTheme.colors.blue,
+                    fontSize: 20,
+                }}>
+                        Convert To Pdf
+                </Text>
+            </TouchableOpacity>
         <ScrollView>
             <View style={{
                 flex: 1,
